99353aeb84e608e89e997b1a90387bfa50bae974
angel <angelxd0714@gmail.com>
2022-01-16 19:59:32 -0500

    cambios

--- ejercicio.js
+++ ejercicio.js
@@ -1,5 +1,6 @@
-let arreglo = ["ropa", "arreglo", "losa"]
+var arreglo = ["ropa", "arreglo", "losa"]
 for (var i = 0; i < arreglo.length; i) {
-    document.write("arreglo: ", arreglo[i]);
+
 
 }
+document.write("arreglo: ", arreglo[i]);
