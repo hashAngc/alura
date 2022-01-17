bff669ad4410f9ad54a0ccc84c68f31eef937b4c
angel <angelxd0714@gmail.com>
2022-01-16 21:58:06 -0500

    ambios

--- ejercicio.js
+++ ejercicio.js
@@ -1,6 +1,26 @@
 var arreglo = ["ropa", "arreglo", "losa"]
-for (var i = 0; i < arreglo.length; i) {
-
+for (var i = 0; i < arreglo.length; i++) {
+    document.write("\n arreglo \n:   ", arreglo[i]);
 
 }
-alert("arreglo: ", arreglo[i]);
+
+var caja1 = [1200];
+var caja2 = [1200];
+sumar = 0
+
+sumar = [caja1[0] + caja1[0]];
+
+document.write("Sumer \t :", sumar);
+
+
+
+/*var persona = {
+    nombre: "angel",
+    apellidos: "sant",
+    celular: 45222,
+    retornar: function() {
+        return this.nombre + "" + this.apellidos;
+    }
+};
+
+document.write(persona.nombre);*/
