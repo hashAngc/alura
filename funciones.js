047866f329d820bb33fceaa029131c077025eab1
angel <angelxd0714@gmail.com>
2022-01-17 20:37:00 -0500

    se esta terminando el encriptado

---
+++ funciones.js
@@ -0,0 +1,96 @@
+var botonEncriptar = document.querySelector("#boton1");
+console.log(botonEncriptar);
+botonEncriptar.addEventListener("click", function(event) {
+    console.log("hizo click");
+    var campo2 = document.querySelector("#campo2").value;
+    console.log(campo2);
+
+});
+
+
+
+
+
+
+
+
+function encriptar(campo2) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var arreglo = ["ropa", "arreglo", "losa"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (var i = 0; i < arreglo.length; i++) {
+    document.write("\n arreglo \n:   ", arreglo[i]);
+
+}
+
+var caja1 = [1200];
+var caja2 = [1200];
+sumar = 0
+
+sumar = [caja1[0] + caja1[0]];
+
+document.write("Sumer \t :", sumar);*/
+
+
+
+/*var persona = {
+    nombre: "angel",
+    apellidos: "sant",
+    celular: 45222,
+    retornar: function() {
+        return this.nombre + "" + this.apellidos;
+    }
+};
+
+document.write(persona.nombre);*/
